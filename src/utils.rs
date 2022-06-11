5aeafe19e3d25e9c93f7d1cd5d2cb1657ff971aa
Jack Lee <jacklee1792@gmail.com>
2022-06-11 15:47:48 -0400

    #7: print_bb -> bb_string

--- src/utils.rs
+++ src/utils.rs
@@ -1,15 +1,17 @@
 use crate::aliases::Bitboard;
 
-pub fn print_bb(bb: Bitboard) {
-    println!("\n  {:#018x}\n", bb);
+pub fn bb_string(bb: Bitboard) -> String {
+    let mut ret = String::new();
+    ret.push_str(&*format!("{:#018x}\n", bb));
     for rank in (0u8..8).rev() {
-        print!("{}  ", rank + 1);
+        ret.push_str(&*format!("{} ", rank + 1));
         for file in 0u8..8 {
             let sq: u8 = 8 * rank + file;
             let bit: u64 = bb & (1u64 << sq);
-            print!("{}", if bit != 0 { "X" } else { "." });
-            print!("{}", if file <= 6 { " " } else { "\n" });
+            ret.push_str(if bit != 0 { "X" } else { "." });
+            ret.push_str(if file <= 6 { " " } else { "\n" });
         }
     }
-    println!("\n   A B C D E F G H\n")
+    ret.push_str("  A B C D E F G H");
+    ret
 }
