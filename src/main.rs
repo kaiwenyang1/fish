8b4e8fa1a204734d7ae19bf29622efe5301141f7
Jack Lee <jacklee1792@gmail.com>
2022-06-11 15:27:30 -0400

    #7: Clear main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,4 @@ mod positions;
 mod tables;
 mod utils;
 
-fn main() {
-    let ms = masks::make_mask_lookup();
-    tables::make_table_set(&ms);
-
-    let p = positions::init_chess();
-    p.print();
-}
+fn main() {}
